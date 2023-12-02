e133caeb8f0664dc0b52437c95bc9916d1704f38
MichaelReid85 <Mike1109r@yahoo.com>
2023-12-02 15:38:00 -0800

    personal touches to finish it up

--- main.py
+++ main.py
@@ -1,34 +1,29 @@
 from tkinter import *
 from tkinter import messagebox
-import random
+from random import randint, choice, shuffle
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
 
-password_list = []
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-for char in range(nr_letters):
-  password_list.append(random.choice(letters))
+    password_letters = [choice(letters) for _ in range(randint(9, 12))]
+    password_symbols = [choice(symbols) for _ in range(randint(3, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(3, 4))]
 
-for char in range(nr_symbols):
-  password_list += random.choice(symbols)
+    password_list = password_numbers + password_symbols + password_letters
+    shuffle(password_list)
 
-for char in range(nr_numbers):
-  password_list += random.choice(numbers)
+    password = "".join(password_list)
+    pw_input.insert(0, password)
+    pyperclip.copy(password)
 
-random.shuffle(password_list)
-
-password = ""
-for char in password_list:
-  password += char
-
-print(f"Your password is: {password}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
@@ -52,48 +47,40 @@ def save():
                 pw_input.delete(0, END)
 
 
-
-
-# Write to the data inside the entries to a data.txt file when the Add button is clicked
-
-# Each website, email, and password combination should be on a new line inside the file.
-
-# All fields need to be cleared after Add button is pressed
-
-
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=50, pady=50, bg="white")
+window.config(padx=50, pady=50, bg="dim gray")
 
-canvas = Canvas(width=200, height=200, bg="white", highlightthickness=0)
+canvas = Canvas(width=300, height=300, bg="dim grey", highlightthickness=0)
 logo_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=logo_img)
+canvas.create_image(130, 150, image=logo_img)
 canvas.grid(row=0, column=1)
 
 # Labels
-web_text = Label(text="Website: ", bg="white")
+web_text = Label(text="Website: ", bg="dim grey", font=("Ariel", 15, "bold"))
 web_text.grid(row=1, column=0, sticky="E")
-email_text = Label(text="Email/Username: ", bg="white")
+email_text = Label(text="Email/Username: ", bg="dim grey", font=("Ariel", 15, "bold"))
 email_text.grid(row=2, column=0, sticky="E")
-pw_text = Label(text="Password: ", bg="white")
+pw_text = Label(text="Password: ", bg="dim grey", font=("Ariel", 15, "bold"))
 pw_text.grid(row=3, column=0, sticky="E")
 
 # Entries
-web_input = Entry(width=35)
+web_input = Entry(width=35, font=("Ariel", 15))
 web_input.grid(row=1, column=1, columnspan=2, sticky="EW")
 web_input.focus()
-email_input = Entry(width=35)
+email_input = Entry(width=35, font=("Ariel", 15))
 email_input.grid(row=2, column=1, columnspan=2, sticky="EW")
 email_input.insert(0, "email@something.com")
-pw_input = Entry(width=21)
+pw_input = Entry(width=21, font=("Ariel", 15))
 pw_input.grid(row=3, column=1, sticky="EW")
 
+
 # Buttons
-add_btn = Button(text="Add",  bg="white", width=35, command=save)
+add_btn = Button(text="Add", bg="lime green", width=35, font=("Ariel", 13, "bold"), command=save)
 add_btn.grid(row=4, column=1, columnspan=2, sticky="EW")
-pw_gen = Button(text="Generate Password", bg="white")
+pw_gen = Button(text="Generate Password", bg="red", font=("Ariel", 13, "bold"), command=generate_password)
 pw_gen.grid(row=3, column=2, sticky="EW")
 
 
