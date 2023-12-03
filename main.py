18668c22868da8da68c4d16a5ac3e400f5bddca6
MichaelReid85 <Mike1109r@yahoo.com>
2023-12-03 11:19:16 -0800

    adding try/except/finally

--- main.py
+++ main.py
@@ -41,8 +41,21 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showerror(title="Oops", message="Please don't leave any fields empty!")
     else:
-        with open("data.json", "w") as data_file:
-            json.dump(new_data, data_file, indent=4)
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
             web_input.delete(0, END)
             pw_input.delete(0, END)
 
